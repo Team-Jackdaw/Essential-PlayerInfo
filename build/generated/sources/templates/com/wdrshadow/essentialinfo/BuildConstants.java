dd79a0cad006acc84a29868f83e6c9722e081370
Jonathan Hsu <63380140+WDRshadow@users.noreply.github.com>
2021-12-03 17:22:44 +0000

    Delete build directory

--- build/generated/sources/templates/com/wdrshadow/essentialinfo/BuildConstants.java
+++
@@ -1,7 +0,0 @@
-package com.wdrshadow.essentialinfo;
-
-// The constants are replaced before compilation
-public class BuildConstants {
-
-    public static final String VERSION = "0.1-SNAPSHOT";
-}
